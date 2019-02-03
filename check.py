325267aed78baff6575117ca9148251ee57f9f3f
Daniel <DanielxBlack@users.noreply.github.com>
2019-02-03 02:22:36 -0600

    Create check.py

---
+++ check.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+
+# Import OS and clear screen
+import os
+os.system("clear")
+
+# Import required modules
+from haveibeenpwnd import check_password
+import getpass
+
+
+
+yourPassword = getpass.getpass("What password do you want to use? ")
+
+meow = check_password(yourPassword)
+
+
+
+pwn_count = meow["count"]
+
+
+if pwn_count > 0:
+    print(f"This password has been pwned {pwn_count} times. Better use different one.")
+else:
+    print(f"Can't find any known pwnage for this password")
